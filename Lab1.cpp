9e09c5360772fbab154337df723455b415faaf52
Jameson54 <hehlol123123@mail.ru>
2025-09-23 22:09:34 +0700

    Внёс изменения

--- Lab1.cpp
+++ Lab1.cpp
@@ -2,34 +2,30 @@
  * Автор: Кондратенко Павел    *
  * Вариант: 7                  *
  *******************************/
+ 
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-    
-  double a = 16.74;
-  
-  double R3 = (a * sqrt(3)) / 3;
-  
-  double R5 = a * sqrt(1 / 2 + sqrt(5) / 10);
-  
-  double R8 = a * sqrt(1 + sqrt(2) / 2);
-  
-  double R10 = (a / 2) * (1 + sqrt(5));
-  
   int n = 6;
+  double a;
+  double R3, R5, R8, R10, Rn;
   
-  double Rn = (a) / (2 * sin(M_PI / n));
+  cout << "a = ";
+  cin  >> a;
   
-  cout <<  R3 << endl;
+  R3 = (a * sqrt(3)) / 3;
+  R5 = a * sqrt(1 / 2 + sqrt(5) / 10);
+  R8 = a * sqrt(1 + sqrt(2) / 2);
+  R10 = (a / 2) * (1 + sqrt(5));
+  Rn = (a) / (2 * sin(M_PI / n));
   
-  cout <<  R5 << endl;
-  
-  cout <<  R8 << endl;
-  
-  cout <<  R10 << endl;
+  cout << "R3 = " << R3  << endl;
+  cout << "R5 = " << R5  << endl;
+  cout << "R8 = " << R8  << endl;
+  cout << "R10 = "<< R10 << endl;
   
   return 0;
 }
