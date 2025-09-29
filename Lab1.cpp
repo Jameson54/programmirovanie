7a0ca78e156268e7b9c137cad890644f9d31b499
Jameson54 <hehlol123123@mail.ru>
2025-09-29 17:55:13 +0700

    Внёс изменения

--- Lab1.cpp
+++ Lab1.cpp
@@ -8,23 +8,22 @@
 using namespace std;
 
 int main() {
-  int n = 6;
+  int n;
   double a;
   double R3, R5, R8, R10, Rn;
 
   cout << "a = ";
   cin  >> a;
+  
+  cin >> n;
 
-  R3 = (a * sqrt(3)) / 3;
-  R5 = a * sqrt(1 / 2 + sqrt(5) / 10);
-  R8 = a * sqrt(1 + sqrt(2) / 2);
-  R10 = (a / 2) * (1 + sqrt(5));
-  Rn = (a) / (2 * sin(M_PI / n));
+  R3 = (a * sqrt(3.0)) / 3.0;
+  R5 = a * sqrt(1.0 / 2.0 + sqrt(5.0) / 10.0);
+  R8 = a * sqrt(1.0 + sqrt(2.0) / 2.0);
+  R10 = a / 2.0 * (1.0 + sqrt(5.0));
+  Rn = a / (2.0 * sin(M_PI / n));
 
-  cout << "R3 = " << R3  << endl;
-  cout << "R5 = " << R5  << endl;
-  cout << "R8 = " << R8  << endl;
-  cout << "R10 = "<< R10 << endl;
+  cout << "R3 = " << R3 << " R5 = " << R5 << " R8 = " << R8 << " R10 = " << R10 << endl;
 
   return 0;
 }
